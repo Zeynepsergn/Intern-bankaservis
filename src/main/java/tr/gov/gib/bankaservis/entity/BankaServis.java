f4af3864a8ac531da335e5f5420f696775ecf1e3
melih.atalay <melih.atalay@gelirler.gov.tr>
2024-08-22 17:39:43 +0300

    @melihatalayy Banka servis türkçe karakter entity hatası

--- src/main/java/tr/gov/gib/bankaservis/entity/BankaServis.java
+++ src/main/java/tr/gov/gib/bankaservis/entity/BankaServis.java
@@ -16,7 +16,7 @@ public class BankaServis {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "banka_servis_id_gen")
     @SequenceGenerator(name = "banka_servis_id_gen", sequenceName = "banka_servis_kullanıcı_id_seq", allocationSize = 1)
-    @Column(name = "\"kullanıcı_id\"", nullable = false)
+    @Column(name = "kullanici_id", nullable = false)
     private Integer id;
 
     @Size(max = 16)
@@ -33,7 +33,7 @@ public class BankaServis {
     @Column(name = "sk_ay")
     private Short skAy;
 
-    @Column(name = "\"sk_yİl\"")
+    @Column(name = "sk_yil")
     private Short skYil;
 
     @Column(name = "bakiye", precision = 10, scale = 2)
